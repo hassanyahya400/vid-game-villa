c95727fc642e0bcf72b6980d9c2ed23eb2c89cad
Hassan Yahya <hassanyahya400@gmail.com>
2024-05-14 23:16:00 +0100

    Implement color mode toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/vid-game-villa-logo.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize={20}/>
-      <Text>Nav bar</Text>
+    <HStack justifyContent="space-between" padding={3}>
+      <Image src={logo} boxSize={20} />
+      <ColorModeSwitch />
     </HStack>
   );
 };
