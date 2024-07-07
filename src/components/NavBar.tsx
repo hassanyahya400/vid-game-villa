3f041da913492e55bb8b0a2bd085c59f71e83600
Hassan Yahya <hassanyahya400@gmail.com>
2024-07-07 03:43:35 +0100

    Add Search Input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/vid-game-villa-logo.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding={3}>
       {/* <Image src={logo} boxSize={20} /> */}
       <Text>VidGame Logo</Text>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
